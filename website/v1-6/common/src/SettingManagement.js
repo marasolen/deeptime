6f4e958d26c9d9e6a25313d0d9821cbdd05c9f38
Mara Solen <marasolen@gmail.com>
2024-01-19 16:28:36 -0800

    pressing anywhere does explore the past, except for buttons or events which revisit

---
+++ website/v1-6/common/src/SettingManagement.js
@@ -0,0 +1,77 @@
+let sheetsId = "";
+
+let user = "";
+let pass = "";
+
+let currentGroupIndex = 0;
+let currentEventIndex = 0;
+
+let backEventAmount = 0;
+let backGroupAmount = 0;
+
+let interactionMode = "interactive";
+let dynamicWait = 60;
+let animationInterval = 2;
+let animationMode = "animation-event";
+let animationResetInterval = 6;
+
+let logInterval = 3600;
+
+let noSettings = true;
+let subtitle = "";
+
+const loadURLSetting = (url, parameter, defaultValue) => {
+    if (url.searchParams.has(parameter)) {
+        noSettings = false;
+        return url.searchParams.get(parameter);
+    } else {
+        url.searchParams.set(parameter, defaultValue);
+        return defaultValue;
+    }
+};
+
+const loadURLSettings = () => {
+    let url = new URL(window.location.href);
+
+    sheetsId = loadURLSetting(url, "id", sheetsId);
+
+    user = loadURLSetting(url, "user", user);
+    pass = decodeURIComponent(loadURLSetting(url, "pass", pass));
+
+    currentGroupIndex = +loadURLSetting(url, "gIndex", currentGroupIndex);
+    currentEventIndex = +loadURLSetting(url, "eIndex", currentEventIndex);
+    backGroupAmount = +loadURLSetting(url, "backGroup", backGroupAmount);
+    backEventAmount = +loadURLSetting(url, "backEvent", backEventAmount);
+    interactionMode = loadURLSetting(url, "mode", "interactive");
+    dynamicWait = +loadURLSetting(url, "dynamicWait", dynamicWait);
+    animationInterval = +loadURLSetting(url, "interval", animationInterval);
+    animationMode = loadURLSetting(url, "animMode", animationMode);
+    animationResetInterval = +loadURLSetting(url, "resetInterval", animationResetInterval);
+    logInterval = +loadURLSetting(url, "logInterval", logInterval);
+    subtitle = decodeURIComponent(loadURLSetting(url, "subtitle", subtitle));
+
+    window.history.pushState("string", "Title", url.href);
+
+    return sheetsId;
+};
+
+const updateURL = () => {
+    let url = new URL(window.location.href);
+
+    url.searchParams.set("id", sheetsId);
+    url.searchParams.set("user", user);
+    url.searchParams.set("pass", encodeURIComponent(pass));
+    url.searchParams.set("gIndex", currentGroupIndex);
+    url.searchParams.set("eIndex", currentEventIndex);
+    url.searchParams.set("backEvent", backEventAmount.toString());
+    url.searchParams.set("backGroup", backGroupAmount.toString());
+    url.searchParams.set("mode", interactionMode);
+    url.searchParams.set("dynamicWait", dynamicWait);
+    url.searchParams.set("interval", animationInterval);
+    url.searchParams.set("animMode", animationMode);
+    url.searchParams.set("resetInterval", animationResetInterval);
+    url.searchParams.set("logInterval", logInterval);
+    url.searchParams.set("subtitle", encodeURIComponent(subtitle))
+
+    window.history.pushState("string", "Title", url.href);
+};
---
+++ website/v1-6/vis/src/Main.js
@@ -0,0 +1,206 @@
+const animationDuration = 2000;
+
+let eventPreview;
+let tieredTimeline;
+let timeline;
+
+const setButtonFunctions = () => {
+    document.onkeydown = (event) => {
+        if (event.key === "Escape") {
+            pressOpenSettings = true;
+        }
+    };
+
+    document.onkeyup = (event) => {
+        if (event.key === "Escape" && pressOpenSettings) {
+            pressOpenSettings = false;
+
+            interactionHandler("keyboard: toggle settings");
+            changePage("settings");
+        }
+    };
+
+    document.getElementById("back-button").onclick = () => {
+        if (interactionMode === "interactive" || interactionMode === "dynamic") {
+            interactionHandler("button: back");
+            backEvent();
+        }
+
+        updateButtonStatuses();
+    };
+
+    document.getElementById("reset-button").onclick = () => {
+        if (interactionMode === "interactive" || interactionMode === "dynamic") {
+            interactionHandler("button: reset");
+            reset();
+        }
+
+        updateButtonStatuses();
+    };
+
+    document.getElementById("next-button").onclick = () => {
+        if (interactionMode === "interactive" || interactionMode === "dynamic") {
+            interactionHandler("button: next event");
+            nextEvent();
+        }
+
+        updateButtonStatuses();
+    };
+
+    document.getElementById("pause-button").onclick = () => {
+        if (interactionMode === "dynamic") {
+            endAnimation();
+        }
+    };
+
+    document.getElementById("wait-button").onclick = () => {
+        if (interactionMode === "dynamic") {
+            cancelAnimation();
+        }
+    };
+};
+
+const setContainerSize = (config) => {
+    config.containerHeight = document.getElementById(config.parentElement.slice(1)).getBoundingClientRect().height;
+    config.containerWidth = document.getElementById(config.parentElement.slice(1)).getBoundingClientRect().width;
+};
+
+let buttonUnclickTimer; 
+const autoClickButton = (buttonId) => {
+    clearTimeout(buttonUnclickTimer)
+    const button = document.getElementById(buttonId)
+    button.click();
+    button.classList.toggle("clicked", true)
+
+    buttonUnclickTimer = setTimeout(() => button.classList.toggle("clicked", false), 60);
+}
+
+const resizeStyles = (config) => {
+    $(config.parentElement).css("border-width", config.borderWidth * window.innerHeight + "px");
+    $(config.parentElement).css("border-radius", 6 * config.borderWidth * window.innerHeight + "px");
+    $(config.parentElement).css("padding-left", 6 * config.borderWidth * window.innerHeight + "px");
+    $(config.parentElement).css("padding-right", 6 * config.borderWidth * window.innerHeight + "px");
+    $(config.parentElement).css("padding-top", 2 * config.borderWidth * window.innerHeight + "px");
+    $(config.parentElement).css("padding-bottom", 2 * config.borderWidth * window.innerHeight + "px");
+    $(config.parentElement).css("font-size", config.descriptionFontSize * window.innerHeight + "px");
+    $(config.parentElement + " input").css("font-size", config.descriptionFontSize * window.innerHeight + "px");
+    $(config.parentElement + " select").css("font-size", config.descriptionFontSize * window.innerHeight + "px");
+    $(config.parentElement + " h1").css("font-size", config.headerFontSize * window.innerHeight + "px");
+    $(config.parentElement + " ul").css("padding-left", config.headerFontSize * window.innerHeight + "px");
+    $(config.parentElement + " ol").css("padding-left", config.headerFontSize * window.innerHeight + "px");
+};
+
+const setContainerSizes = () => {
+    setContainerSize(tieredTimelineConfig);
+    setContainerSize(timelineConfig);
+    setContainerSize(mediaBoxConfig);
+
+    $("#separator").css("border-width", mediaBoxConfig.borderWidth * window.innerHeight + "px");
+    resizeStyles(mediaBoxConfig);
+    resizeStyles(settingsConfig);
+
+    $(".interface-button").css("font-size", 0.8 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#next-button").css("font-size", 1.3 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#wait-button").css("font-size", 1.3 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#pause-button").css("font-size", 1.3 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $(".interface-button").css("border-radius", 2 * mediaBoxConfig.borderWidth * window.innerHeight + "px");
+    $(".interaction-description").css("font-size", 0.75 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#title").css("font-size", 1.4 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#btitle").css("font-size", mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#subtitle").css("font-size", 1.4 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+    $("#btitle b").css("font-size", 1.1 * mediaBoxConfig.headerFontSize * window.innerHeight + "px");
+};
+
+window.addEventListener('load', async () => {
+    const absorbEvent = event => event.preventDefault()
+    document.addEventListener('contextmenu', absorbEvent); 
+
+    loadURLSettings();
+    if (!sheetsId) {
+        changePage("welcome");
+        return;
+    }
+
+    await getData();
+    if (!data) {
+        changePage("settings");
+        return;
+    }
+    
+    if (interactionMode === "animated") {
+        document.getElementById("interaction-container").style.display = "none";
+        document.getElementById("media-focus").style.height = "55%";
+        startAnimation(true);
+    } else if (interactionMode === "dynamic") {
+        initializeDynamicAnimation();
+    }
+
+    setButtonFunctions();
+    setContainerSizes();
+
+    const dataCopy = getSlicedData();
+
+    tieredTimeline = new TieredTimeline(tieredTimelineConfig, dataCopy);
+
+    const numEventsInGroup = backGroupAmount === 0 ? currentEventIndex : data[currentGroupIndex - backGroupAmount].events.length - 1;
+    timeline = new Timeline(timelineConfig,
+        {
+            label: data[data.length - 1].label,
+            time: data[data.length - 1].time
+        },
+        dataCopy,
+        {
+            label: data[currentGroupIndex - backGroupAmount].events[numEventsInGroup - backEventAmount].label,
+            time: data[currentGroupIndex - backGroupAmount].events[numEventsInGroup - backEventAmount].time
+        });
+
+    const event = data[currentGroupIndex - backGroupAmount].events[numEventsInGroup - backEventAmount];
+    setMedia(event.label, event.description, event.image);
+    if (backEventAmount > 0 || backGroupAmount > 0) {
+        setTimeout(() => tieredTimeline.setBoldedEvent(currentGroupIndex - backGroupAmount, numEventsInGroup - backEventAmount), 50);
+    }
+
+    updateButtonStatuses();
+
+    document.getElementById("tap-handler").addEventListener("click", () => {
+        autoClickButton("next-button");
+    });
+
+    document.getElementById("wait-tap-handler").addEventListener("click", () => {
+        autoClickButton("wait-button");
+    });
+
+    document.getElementById("pause-tap-handler").addEventListener("click", () => {
+        autoClickButton("pause-button");
+    });
+
+    $('img').on('dragstart', function(event) { event.preventDefault(); });
+
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("escape-to-settings").style.display = "block";
+
+    if (subtitle) {
+        document.getElementById("subtitle").innerHTML = subtitle;
+    } else {
+        document.getElementById("titles").style.textAlign = "center";
+    }
+
+    setTimeout(() => {
+        document.getElementById("escape-to-settings").style.display = "none";
+    }, 3000);
+});
+
+window.addEventListener('resize', () => {
+    setContainerSizes();
+
+    tieredTimeline.config = tieredTimelineConfig;
+    tieredTimeline.setupChart(0);
+
+    timeline.config = timelineConfig;
+    timeline.setupChart();
+
+    const numEventsInGroup = backGroupAmount === 0 ? currentEventIndex : data[currentGroupIndex - backGroupAmount].events.length - 1;
+    if (backEventAmount > 0 || backGroupAmount > 0) {
+        setTimeout(() => tieredTimeline.setBoldedEvent(currentGroupIndex - backGroupAmount, numEventsInGroup - backEventAmount), 50);
+    }
+});
